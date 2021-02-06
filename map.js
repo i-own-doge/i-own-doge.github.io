131e627bd4b7435694a98f219c929478b65678e7
i-own-doge <78620053+i-own-doge@users.noreply.github.com>
2021-02-05 23:37:05 -0500

    Update map.js

--- map.js
+++ map.js
@@ -4,14 +4,25 @@ var map = L.mapbox.map('map')
     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
 
 
-for(var pair in coordPairs) {
-  var coords = coordPairs[pair];
-  var generator = new arc.GreatCircle(coords.from, coords.to, { name: pair });
-  var line = generator.Arc(100, { offset: 10 });
-  L.geoJson(line.json(), {
-    style: {
-      color: "#" + Math.floor(Math.random()*16777215).toString(16),
-      weight: 1
-    }
-  }).addTo(map);
+for(var toCountry in coordPairs) {
+  var color = "#" + Math.floor(Math.random()*16777215).toString(16);
+  var config = coordPairs[toCountry];
+
+  for(var fromCountry in config) {
+    var pairConfig = config[fromCountry];
+    var toLangFull = langsAndCountries[pairConfig.toLang].full;
+    var fromLangFull = langsAndCountries[pairConfig.fromLang].full;
+    var it_says = fromLangFull + ' might say: it is all ' + toLangFull + ' to me!';
+    var generator = new arc.GreatCircle(pairConfig.from, pairConfig.to, { name: it_says });
+    var line = generator.Arc(100, { offset: 10 });
+    L.geoJson(line.json(), {
+      style: {
+        color: color,
+        opacity: 0.7,
+        weight: 2
+      }
+    }).bindPopup(function (layer) {
+      return layer.feature.properties.name;
+    }).addTo(map);
+  }
 }
